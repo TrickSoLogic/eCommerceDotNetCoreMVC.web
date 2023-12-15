392e3e38827e46a94ae9e665b72a93a939850595
TrickSoLogic <118069084+TrickSoLogic@users.noreply.github.com>
2023-12-15 19:53:46 +0530

    update 5.3

---
+++ Models/Transaction.cs
@@ -0,0 +1,11 @@
+﻿namespace eCommerceDotNetCoreMVC.web.Models
+{
+    public class Transaction
+    {
+        public int TransactionId { get; set; }
+        public string UserId { get; set; } // Link to the User model
+        public int OrderId { get; set; } // Link to the Order model
+        public decimal Amount { get; set; }
+        public DateTime TransactionDate { get; set; }
+     }
+}
