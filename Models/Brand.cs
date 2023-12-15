5c422254ca08f5d323d40875eb74d6b76cb4e5ff
TrickSoLogic <118069084+TrickSoLogic@users.noreply.github.com>
2023-12-15 19:51:06 +0530

    update 5.0

---
+++ Models/Brand.cs
@@ -0,0 +1,8 @@
+﻿namespace eCommerceDotNetCoreMVC.web.Models
+{
+    public class Brand
+    {
+        public int BrandId { get; set; }
+        public string Name { get; set; }
+     }
+}
---
+++ Models/Review.cs
@@ -0,0 +1,11 @@
+﻿namespace eCommerceDotNetCoreMVC.web.Models
+{
+    public class Review
+    {
+        public int ReviewId { get; set; }
+        public string UserId { get; set; } // Link to the User model
+        public int ProductId { get; set; } // Link to the Product model
+        public int Rating { get; set; }
+        public string Comment { get; set; }
+     }
+}
