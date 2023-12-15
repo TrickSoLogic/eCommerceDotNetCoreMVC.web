4924619af39c2067940b712d8b84196575d3eba8
TrickSoLogic <118069084+TrickSoLogic@users.noreply.github.com>
2023-12-15 20:00:03 +0530

    update 7.0

---
+++ Models/Payment.cs
@@ -0,0 +1,10 @@
+﻿namespace eCommerceDotNetCoreMVC.web.Models
+{
+    public class Payment
+    {
+        public int PaymentId { get; set; }
+        public string UserId { get; set; } // Link to the User model
+        public decimal Amount { get; set; }
+        public DateTime PaymentDate { get; set; }
+     }
+}
